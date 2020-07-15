8de6bf7cc31538782b2dbd5fa7a32d829db2a3be
tekeshpixel@gmail.com <tekeshpixel@gmail.com>
2020-07-15 23:06:11 +0530

    call api

---
+++ api/members.js
@@ -0,0 +1,16 @@
+const members =[
+    {
+        id:1,
+        name:'Tekesh',
+        email:'tekesh@gmail.com',
+        status:'active'        
+    },
+    {
+        id:2,
+        name:'Rahul',
+        email:'rahul@gmail.com',
+        status:'active'        
+    }
+];
+
+module.exports=members;
